2c1b3aad5b87f2033a066837b84524c42baccb5d
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-12-24 08:26:12 +1100

    Prepare for Award data type.

--- src/main/java/net/modgarden/backend/data/award/Award.java
+++ src/main/java/net/modgarden/backend/data/award/Award.java
@@ -2,7 +2,7 @@ package net.modgarden.backend.data.award;
 
 import org.jetbrains.annotations.Nullable;
 
-public record Award(AwardType type,
+public record Award(String typeId,
                     @Nullable String additionalTooltip) {
 
 }
