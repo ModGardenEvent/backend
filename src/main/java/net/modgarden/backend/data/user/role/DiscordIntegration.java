108714c7c171ad92b79f0cd99307c294326d6b99
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-03-26 22:05:39 +1100

    feat!: Swap out `color` field for `role_id` in Discord role integration.

--- src/main/java/net/modgarden/backend/data/user/role/DiscordIntegration.java
+++ src/main/java/net/modgarden/backend/data/user/role/DiscordIntegration.java
@@ -4,9 +4,9 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.modgarden.backend.data.Integration;
 
-public record DiscordIntegration(String color, String permissions) implements Integration {
+public record DiscordIntegration(String roleId, String permissions) implements Integration {
 	public static final Codec<DiscordIntegration> CODEC = RecordCodecBuilder.create(inst -> inst.group(
-			Codec.STRING.fieldOf("color").forGetter(DiscordIntegration::color),
+			Codec.STRING.fieldOf("role_id").forGetter(DiscordIntegration::roleId),
 			Codec.STRING.fieldOf("permissions").forGetter(DiscordIntegration::permissions)
 	).apply(inst, DiscordIntegration::new));
 
