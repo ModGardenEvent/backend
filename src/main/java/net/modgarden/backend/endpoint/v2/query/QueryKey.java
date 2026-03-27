734ef236dcc8c79dfc9d91800ef2f8cc864dbcbe
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2026-03-27 20:19:25 +1100

    fix: Throw NotFoundException if QueryKey or QueryValue can't be found.

--- src/main/java/net/modgarden/backend/endpoint/v2/query/QueryKey.java
+++ src/main/java/net/modgarden/backend/endpoint/v2/query/QueryKey.java
@@ -3,6 +3,8 @@ package net.modgarden.backend.endpoint.v2.query;
 import java.util.Locale;
 
 import io.javalin.http.Context;
+import net.modgarden.backend.endpoint.exception.HypertextException;
+import net.modgarden.backend.endpoint.exception.NotFoundException;
 
 /// An enum representing what type of key to query a value by.
 public enum QueryKey {
@@ -14,7 +16,7 @@ public enum QueryKey {
 	USERNAME,
 	UNDEFINED;
 
-	public static QueryKey fromQuery(Context ctx, QueryKey defaultKey) {
+	public static QueryKey fromQuery(Context ctx, QueryKey defaultKey) throws HypertextException {
 		QueryKey queryKey = fromQuery(ctx);
 
 		if (queryKey.equals(UNDEFINED)) {
@@ -24,13 +26,17 @@ public enum QueryKey {
 		}
 	}
 
-	public static QueryKey fromQuery(Context ctx) {
+	public static QueryKey fromQuery(Context ctx) throws HypertextException {
 		String param = ctx.queryParam(QueryParameterType.get(QueryKey.class).toString());
 
 		if (param == null) {
 			return UNDEFINED;
 		}
 
-		return QueryKey.valueOf(QueryKey.class, param.toUpperCase(Locale.ROOT));
+		try {
+			return QueryKey.valueOf(QueryKey.class, param.toUpperCase(Locale.ROOT));
+		} catch (IllegalArgumentException e) {
+			throw new NotFoundException("Query key context '" + param + "' does not exist");
+		}
 	}
 }
--- src/main/java/net/modgarden/backend/endpoint/v2/query/QueryValue.java
+++ src/main/java/net/modgarden/backend/endpoint/v2/query/QueryValue.java
@@ -3,6 +3,8 @@ package net.modgarden.backend.endpoint.v2.query;
 import java.util.Locale;
 
 import io.javalin.http.Context;
+import net.modgarden.backend.endpoint.exception.HypertextException;
+import net.modgarden.backend.endpoint.exception.NotFoundException;
 
 /// An enum representing what type of value to return from a query.
 public enum QueryValue {
@@ -11,7 +13,7 @@ public enum QueryValue {
 	SLUG,
 	UNDEFINED;
 
-	public static QueryValue fromQuery(Context ctx, QueryValue defaultValue) {
+	public static QueryValue fromQuery(Context ctx, QueryValue defaultValue) throws HypertextException {
 		QueryValue queryValue = fromQuery(ctx);
 
 		if (queryValue.equals(UNDEFINED)) {
@@ -21,13 +23,16 @@ public enum QueryValue {
 		}
 	}
 
-	public static QueryValue fromQuery(Context ctx) {
+	public static QueryValue fromQuery(Context ctx) throws HypertextException {
 		String param = ctx.queryParam(QueryParameterType.get(QueryValue.class).toString());
 
 		if (param == null) {
 			return UNDEFINED;
 		}
-
-		return QueryValue.valueOf(QueryValue.class, param.toUpperCase(Locale.ROOT));
+		try {
+			return QueryValue.valueOf(QueryValue.class, param.toUpperCase(Locale.ROOT));
+		} catch (IllegalArgumentException e) {
+			throw new NotFoundException("Query key context '" + param + "' does not exist");
+		}
 	}
 }
